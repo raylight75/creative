d6a780ba5d63e0a83a1f8520d6b1ba7afe4764d7
raylight75@gmail.com <raylight75@gmail.com>
2017-06-18 12:54:00 +0300

    manage blog sliders

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
-
-# Create your views here.
+# from zinnia.views.archives import EntryIndex
+# from backend.models import SiteImage
+#
+#
+# class CustomWeblog(EntryIndex):
+#     template_name = 'zinnia/template.html'
+#
+#     def get_context_data(self, **kwargs):
+#         # Call the base implementation first to get a context
+#         context = super(EntryIndex, self).get_context_data(**kwargs)
+#         # Add in a QuerySet of all the sliders
+#         context['slider'] = SiteImage.objects.get(cat_id=5)
+#         return context
